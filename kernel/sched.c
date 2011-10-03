66875cfb7e3b75974fc6843f0dd831e2a75a87c0
Benedikt Boss <vork.android@gmail.com>
2011-10-03 11:21:45 -0400

    Add bfs 404 bc Con Kolivas

--- kernel/sched.c
+++ kernel/sched.c
@@ -1,3 +1,6 @@
+#ifdef CONFIG_SCHED_BFS
+#include "sched_bfs.c"
+#else
 /*
  *  kernel/sched.c
  *
@@ -9371,3 +9374,4 @@ void synchronize_sched_expedited(void)
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
 
 #endif /* #else #ifndef CONFIG_SMP */
+#endif /* CONFIG_SCHED_BFS */
